c1ad0f7053b7e9e9ecb240ae0e8c55991a6a9c74
Victor Martins <vitorchiesa74@gmail.com>
2023-05-25 20:33:14 -0300

    commit init

---
+++ js/custom.js
@@ -0,0 +1,163 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        /*---------------------------------------------------
+            slider carousel
+        ----------------------------------------------------*/
+        $('.home-carousel').owlCarousel({
+            loop: true,
+            navText: ['<i class="icofont-simple-left"></i>', '<i class="icofont-simple-right"></i>'],
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            smartSpeed: 450,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            product carousel
+        ----------------------------------------------------*/
+        $('.product-gallery').owlCarousel({
+            loop: true,
+            navText: ['<i class="icofont-simple-left"></i>', '<i class="icofont-simple-right"></i>'],
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            smartSpeed: 450,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            testimonial carousel
+        ----------------------------------------------------*/
+        $('.testi-carousel').owlCarousel({
+            loop: true,
+            navText: ['<i class="icofont-simple-left"></i>', '<i class="icofont-simple-right"></i>'],
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            smartSpeed: 450,
+            margin: 20,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                991: {
+                    items: 3
+                },
+                1200: {
+                    items: 3
+                },
+                1920: {
+                    items: 3
+                }
+            }
+        });
+
+
+        /*---------------------------------------------------
+                magnific popup activation
+        ----------------------------------------------------*/
+        $('.popup-video').magnificPopup({
+            type: 'video'
+        });
+
+        /*---------------------------------------------------
+            scrollIt plugin activation
+        ----------------------------------------------------*/
+        $.scrollIt();
+
+    });
+
+
+    /*---------------------------------------------------
+        smooth scroll
+    ----------------------------------------------------*/
+    $('a[href*="#"]:not([href="#"]):not([data-toggle])').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    /*---------------------------------------------------
+        accordian
+    ----------------------------------------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+    /*---------------------------------------------------
+        sticky header
+    ----------------------------------------------------*/
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 100) {
+            $("#header").removeClass("sticky");
+        } else {
+            $("#header").addClass("sticky");
+        }
+    });
+
+    /*---------------------------------------------------
+        preloader
+    ----------------------------------------------------*/
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(500);
+    });
+
+
+}(jQuery));
